e61fd82bc6cd5cd93e1b23bbb0244a3e7819fcb8
Jono <jono.muamba@outlook.com>
2022-05-11 11:52:22 +0200

    angular-training

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ Run `tsc --init` to generate tsconfig.json
 You won't need to add the compiler flag after you confirgure  tsconfig.json
 
 Eg: `tsc --target ES2016 --noEmitOnError Customer.ts`
+
+To create a new project to existing folder 
+run  `ng new appName --directory ./`
+
+Install Bootstrap to your project
+run `npm i bootstrap` and configure it on your angular.json file under
+"styles": ["./node_modules/bootstrap/dist/css/bootstrap.min.css", "src/styles.css" ],
---
+++ angular-training/01-angular-create-new-component-basic-html-table/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Jono", "Muks", "jono.muks@angular.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@angular.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@angular.com", 90000),
+    new SalesPerson("Mai", "Truong", "mai.truong@angular.com", 60000),
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
